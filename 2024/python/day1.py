e42d9fa4b208f45fa3b9df6105897159dbbd6796
Harsh Narayan Jha <harshnj@proton.me>
2024-12-01 20:07:23 +0530

    day1 part2 in python

--- 2024/python/day1.py
+++ 2024/python/day1.py
@@ -1,7 +1,10 @@
+from collections import Counter
+
+# part 1
 list1 = []
 list2 = []
 
-with open("input1.txt", 'r') as inp:
+with open("input1.txt", "r") as inp:
     while line := inp.readline():
         n, m = line.split()
         list1.append(int(n))
@@ -16,3 +19,12 @@ for j, k in zip(list1, list2):
     dist_sum += abs(j - k)
 
 print(dist_sum)
+
+# part 2
+list2_counts = Counter(list2)
+similarity_score = 0
+
+for k in list1:
+    similarity_score += k * list2_counts[k]
+
+print(similarity_score)
