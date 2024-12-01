86144a30bad1f77779d4cfe5c8a0b1b282e920fb
Harsh Narayan Jha <harshnj@proton.me>
2024-12-01 21:24:07 +0530

    day1 part2 in cpp

--- 2024/cpp/day1.cpp
+++ 2024/cpp/day1.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -59,4 +60,49 @@ int day1() {
   return dist_sum;
 }
 
-int day2() { return 1; }
+int day2() {
+  string line;
+  ifstream input;
+  input.open("../input1.txt", ios::in);
+
+  vector<int> list1;
+  unordered_map<int, int> map2;
+
+  if (input.is_open()) {
+    while (getline(input, line)) {
+      stringstream ss(line);
+
+      string s;
+      int i = 0;
+      while (ss >> s) {
+        if (i == 0) {
+          list1.push_back(stoi(s));
+          i = 1;
+        } else {
+          int value = stoi(s);
+          if (map2.count(value)) {
+            map2[value]++;
+          } else {
+            map2[value] = 1;
+          }
+          i = 0;
+        }
+      }
+    }
+    input.close();
+  }
+
+  size_t i = 0;
+
+  int sim_score = 0;
+
+  while (i < list1.size()) {
+    int value = list1.at(i);
+    if (map2.count(value)) {
+      sim_score += value * map2[value];
+    }
+    i++;
+  }
+
+  return sim_score;
+}
