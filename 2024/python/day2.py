cfc829575289516cc1f72c7fa0a211fb37d13de1
Harsh Narayan Jha <harshnj@proton.me>
2024-12-02 10:43:16 +0530

    day2 part1 in python

---
+++ 2024/python/day2.py
@@ -0,0 +1,38 @@
+def is_report_safe(report: list[int]) -> bool:
+    increasing_safes = [
+        report[i + 1] > report[i] and (report[i + 1] - report[i]) <= 3
+        for i in range(0, len(report) - 1)
+    ]
+    decreasing_safes = [
+        report[i + 1] < report[i] and (report[i] - report[i + 1]) <= 3
+        for i in range(0, len(report) - 1)
+    ]
+
+    return all(increasing_safes) or all(decreasing_safes)
+
+
+def part1() -> int:
+    reports = []
+
+    with open("../data/input2.txt") as inp:
+        lines = inp.readlines()
+        for line in lines:
+            reports.append(list(map(int, line.split())))
+
+    results = []
+
+    for report in reports:
+        if is_report_safe(report):
+            results.append(True)
+        else:
+            results.append(False)
+
+    return results.count(True)
+
+
+def part2() -> int:
+    ...
+
+
+print(part1())
+print(part2())
