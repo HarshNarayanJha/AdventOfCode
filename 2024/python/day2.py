d047f6dcfabf2181240f9650f3fb066850d365ec
Harsh Narayan Jha <harshnj@proton.me>
2024-12-02 11:06:05 +0530

    day2 part2 in python

--- 2024/python/day2.py
+++ 2024/python/day2.py
@@ -31,7 +31,32 @@ def part1() -> int:
 
 
 def part2() -> int:
-    ...
+    reports = []
+
+    with open("../data/input2.txt") as inp:
+        lines = inp.readlines()
+        for line in lines:
+            reports.append(list(map(int, line.split())))
+
+    results = []
+
+    for report in reports:
+        if (
+            is_report_safe(report)
+            or is_report_safe(results[1:])
+            or is_report_safe(results[:-1])
+        ):
+            results.append(True)
+            continue
+
+        for i, level in enumerate(report):
+            if is_report_safe(report[:i] + report[i + 1 :]):
+                results.append(True)
+                break
+        else:
+            results.append(False)
+
+    return results.count(True)
 
 
 print(part1())
