09a3b0e92aeb90fdb55b2dc70a5e68e39bbc1fc6
Harsh Narayan Jha <harshnj@proton.me>
2024-12-03 10:54:12 +0530

    day3 part2 in python

--- 2024/python/day3.py
+++ 2024/python/day3.py
@@ -1,9 +1,10 @@
 import re
 
+
 def part1() -> int:
     instruction = None
     with open("../data/input3.txt") as inp:
-        instruction = ''.join(inp.readlines())
+        instruction = "".join(inp.readlines())
 
     pat = re.compile(r"mul\((\d+),(\d+)\)")
     matches = [(int(i), int(j)) for i, j in pat.findall(instruction)]
@@ -13,7 +14,34 @@ def part1() -> int:
     return result
 
 
-def part2() -> int: ...
+def part2() -> int:
+    instruction = None
+    with open("../data/input3.txt") as inp:
+        instruction = "".join(inp.readlines())
+
+    do_pat = re.compile(r"do\(\)")
+    dont_pat = re.compile(r"don\'t\(\)")
+    pat = re.compile(r"mul\((\d+),(\d+)\)")
+
+    muls = pat.finditer(instruction)
+    dos = do_pat.finditer(instruction)
+    donts = dont_pat.finditer(instruction)
+
+    enabled = True
+
+    result = 0
+
+    arr = sorted(list(muls) + list(dos) + list(donts), key=lambda x: x.span()[0])
+
+    for m in arr:
+        if m.re == pat and enabled:
+            result += int(m.groups()[0]) * int(m.groups()[1])
+        elif m.re == do_pat:
+            enabled = True
+        elif m.re == dont_pat:
+            enabled = False
+
+    return result
 
 
 print(part1())
