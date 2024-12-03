134a9f17b65ccbbd367641e6a92ab0ef02bb4f25
Harsh Narayan Jha <harshnj@proton.me>
2024-12-03 11:27:53 +0530

    day3 part2 in c++

--- 2024/cpp/day3.cpp
+++ 2024/cpp/day3.cpp
@@ -82,4 +82,71 @@ int part1() {
   return result;
 }
 
-int part2() { return 0; }
+int part2() {
+  string line;
+  string instruction;
+  ifstream input("../data/input3.txt", ios::in);
+
+  if (input.is_open()) {
+
+    while (getline(input, line)) {
+      instruction.append(line);
+    }
+
+    input.close();
+  }
+
+  int result = 0;
+  bool enabled = true;
+
+  for (size_t i = 0; i < instruction.length(); i++) {
+    bool match_mul = false;
+
+    size_t j = i + 4;
+    size_t comma = -1;
+
+    // check if matches mul format
+    if (instruction.at(i) == 'm' && instruction.substr(i + 1, 3) == "ul(") {
+
+      while (j < instruction.length()) {
+        if (instruction.at(j) != ',' && !isdigit(instruction.at(j)) &&
+            instruction.at(j) != ')') {
+          break;
+        }
+
+        if (instruction.at(j) == ',') {
+          comma = j;
+        }
+        if (instruction.at(j) == ')') {
+          match_mul = true;
+          break;
+        }
+        j++;
+      }
+    } else if (instruction.at(i) == 'd' &&
+               instruction.substr(i + 1, 3) == "o()") {
+      enabled = true;
+    } else if (instruction.at(i) == 'd' &&
+               instruction.substr(i + 1, 6) == "on't()") {
+      enabled = false;
+    }
+
+    if (!match_mul)
+      continue;
+
+    if (!enabled)
+      continue;
+
+    size_t b_open = i + 3;
+    size_t b_close = j;
+
+    int n1, n2;
+
+    n1 = stoi(instruction.substr(b_open + 1, comma - (b_open + 1)));
+    n2 = stoi(instruction.substr(comma + 1, b_close - (comma + 1)));
+
+    result += n1 * n2;
+  }
+
+  return result;
+}
