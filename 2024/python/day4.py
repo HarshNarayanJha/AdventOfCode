ad626d1537910ab31d3c6b5d4c6648e8531c5dc5
Harsh Narayan Jha <harshnj@proton.me>
2024-12-04 12:44:10 +0530

    day4 part2 in python

--- 2024/python/day4.py
+++ 2024/python/day4.py
@@ -23,7 +23,34 @@ def part1() -> int:
     return count
 
 
-def part2() -> int: ...
+def part2() -> int:
+    grid = []
+    with open("../data/input4.txt") as inp:
+        for line in inp.readlines():
+            grid.append(list(line.strip()))
+
+    count = 0
+
+    valid_coords = set((i, j) for i in range(len(grid)) for j in range(len(grid[0])))
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if (x + 2, y + 2) not in valid_coords:
+                continue
+
+            if grid[x + 1][y + 1] != "A":
+                continue
+
+            if [grid[x + n][y + n] for n in range(3)] in [
+                list("MAS"),
+                list("SAM"),
+            ] and [grid[x + n][y + 2 - n] for n in range(3)] in [
+                list("MAS"),
+                list("SAM"),
+            ]:
+                count += 1
+
+    return count
 
 
 print(part1())
