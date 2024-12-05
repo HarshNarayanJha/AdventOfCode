40b90248d48546ab6588faeee47505841c780d97
Harsh Narayan Jha <harshnj@proton.me>
2024-12-05 12:02:10 +0530

    refactors

--- 2024/python/day5.py
+++ 2024/python/day5.py
@@ -1,35 +1,35 @@
+def parse_input(lines: list[str]) -> tuple[dict[int, tuple[int]], list[list[int]]]:
+    rules = {}
+    orders = []
+
+    rules_done = False
+    for line in lines:
+        if not line.strip():
+            rules_done = True
+            continue
+        if not rules_done:
+            bef, after = tuple(map(int, line.strip().split("|")))
+            rules.setdefault(bef, [])
+            rules[bef].append(after)
+        else:
+            orders.append(list(map(int, line.strip().split(","))))
+
+    return rules, orders
+
+
 def part1() -> int:
     rules = {}
     orders = []
     with open("../data/input5.txt") as inp:
-        lines = inp.readlines()
-
-        rules_done = False
-        for line in lines:
-            if not line.strip():
-                rules_done = True
-                continue
-            if not rules_done:
-                bef, after = tuple(map(int, line.strip().split("|")))
-                if bef in rules:
-                    rules[bef].append(after)
-                else:
-                    rules[bef] = list()
-                    rules[bef].append(after)
-            else:
-                orders.append(tuple(map(int, line.strip().split(","))))
+        rules, orders = parse_input(inp.readlines())
 
     valids = []
-
     for order in orders:
         correct = True
 
         for i, page in enumerate(order):
             if page not in rules:
                 continue
-            # if not all(x in rules[page] for x in order[i + 1 :]):
-            #     correct = False
-            #     break
             if any(x in rules[page] for x in order[:i]):
                 correct = False
                 break
@@ -37,37 +37,16 @@ def part1() -> int:
         if correct:
             valids.append(order)
 
-    middle_sum = 0
-    for v in valids:
-        middle_sum += v[len(v) // 2]
-
-    return middle_sum
+    return sum([v[len(v) // 2] for v in valids])
 
 
 def part2() -> int:
     rules = {}
     orders = []
     with open("../data/input5.txt") as inp:
-        lines = inp.readlines()
-
-        rules_done = False
-        for line in lines:
-            if not line.strip():
-                rules_done = True
-                continue
-            if not rules_done:
-                bef, after = tuple(map(int, line.strip().split("|")))
-                if bef in rules:
-                    rules[bef].append(after)
-                else:
-                    rules[bef] = list()
-                    rules[bef].append(after)
-            else:
-                orders.append(list(map(int, line.strip().split(","))))
+        rules, orders = parse_input(inp.readlines())
 
     invalids = []
-    invalid_indices = []
-
     for order in orders:
         correct = True
 
@@ -75,14 +54,11 @@ def part2() -> int:
             if page not in rules:
                 continue
             if any(x in rules[page] for x in order[:i]):
-                invalid_indices.append(i)
                 correct = False
                 break
 
         if not correct:
             invalids.append(order)
-
-    # now fix invalids (sort)
 
     for inv in invalids:
         for i in range(len(inv)):
@@ -92,11 +68,7 @@ def part2() -> int:
                 if inv[j - 1] in rules[inv[j]]:
                     inv[j], inv[j - 1] = inv[j - 1], inv[j]
 
-    middle_sum = 0
-    for v in invalids:
-        middle_sum += v[len(v) // 2]
-
-    return middle_sum
+    return sum([v[len(v) // 2] for v in invalids])
 
 
 print(part1())
