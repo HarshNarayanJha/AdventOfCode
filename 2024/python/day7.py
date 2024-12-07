e3113631b6bb4a3b3d69194f65554156f737d0d4
Harsh Narayan Jha <harshnj@proton.me>
2024-12-07 10:52:22 +0530

    day7 part1 and 2 in python... part2 is slow

---
+++ 2024/python/day7.py
@@ -0,0 +1,75 @@
+from itertools import product
+
+
+def part1() -> int:
+    equations = {}
+    with open("../data/input7.txt", "r") as inp:
+        lines = inp.readlines()
+        for line in lines:
+            res, values = line.split(":")
+            equations[int(res)] = tuple(map(int, values.split()))
+
+    operators = ["+", "*"]
+
+    true_sum = 0
+
+    for res in equations:
+        vals = equations[res]
+        found = False
+
+        for ops_combo in product(operators, repeat=len(vals) - 1):
+            curr = vals[0]
+            for i, op in enumerate(ops_combo):
+                if op == "+":
+                    curr = curr + vals[i + 1]
+                elif op == "*":
+                    curr = curr * vals[i + 1]
+
+            if curr == res:
+                found = True
+                break
+
+        if found:
+            true_sum += res
+
+    return true_sum
+
+
+def part2() -> int:
+    equations = {}
+    with open("../data/input7.txt", "r") as inp:
+        lines = inp.readlines()
+        for line in lines:
+            res, values = line.split(":")
+            equations[int(res)] = tuple(map(int, values.split()))
+
+    operators = ["+", "*", "||"]
+
+    true_sum = 0
+
+    for res in equations:
+        vals = equations[res]
+        found = False
+
+        for ops_combo in product(operators, repeat=len(vals) - 1):
+            curr = vals[0]
+            for i, op in enumerate(ops_combo):
+                if op == "+":
+                    curr = curr + vals[i + 1]
+                elif op == "*":
+                    curr = curr * vals[i + 1]
+                elif op == "||":
+                    curr = int(str(curr) + str(vals[i + 1]))
+
+            if curr == res:
+                found = True
+                break
+
+        if found:
+            true_sum += res
+
+    return true_sum
+
+
+print(part1())
+print(part2())
