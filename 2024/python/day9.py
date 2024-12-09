8a6276517ff1d33044b4c7888420677e91b613d8
Harsh Narayan Jha <harshnj@proton.me>
2024-12-09 12:21:30 +0530

    day9 part2 in python

--- 2024/python/day9.py
+++ 2024/python/day9.py
@@ -1,8 +1,7 @@
 def part1() -> int:
     disk = None
     with open("../data/input9.txt", "r") as inp:
-        lines = inp.readlines()
-        disk = "".join([line.strip() for line in lines])
+        disk = inp.read().strip()
 
     fs = []
     fid = 0
@@ -16,7 +15,6 @@ def part1() -> int:
         i = (i + 1) % 2
 
     # now defragment it
-
     for b in range(len(fs)):
         if fs[b] == -1:
             idx = -1
@@ -35,7 +33,70 @@ def part1() -> int:
     return res
 
 
-def part2() -> int: ...
+def part2() -> int:
+    disk = None
+    with open("../data/input9.txt", "r") as inp:
+        disk = inp.read().strip()
+
+    fs = []
+    fid = 0
+    i = 0
+    for b in disk:
+        if i == 0:
+            fs.extend([fid] * int(b))
+            fid += 1
+        elif i == 1:
+            fs.extend([-1] * int(b))
+        i = (i + 1) % 2
+
+    # defragment whole disk, file in groups
+    b = len(fs) - 1
+    while b >= 0:
+        if fs[b] != -1:
+            # start of file
+            # calculate file size
+            file_from = b
+            file_to = file_from
+            fid = fs[file_from]
+            while fs[file_to] == fid:
+                file_to -= 1
+            fsize = file_from - file_to
+
+            # search for empty space before the start of the current file
+            empty_from = 0
+            esize = 0
+            while empty_from < b:
+                while empty_from < b and fs[empty_from] != -1:
+                    empty_from += 1
+
+                # calculate it's size
+                while empty_from + esize < b and fs[empty_from + esize] == -1:
+                    esize += 1
+
+                if esize >= fsize:
+                    break
+
+                empty_from += 1
+                esize = 0
+
+            if fsize <= esize:
+                # move blocks
+                for i in range(fsize):
+                    fs[empty_from + i], fs[file_from - i] = (
+                        fs[file_from - i],
+                        fs[empty_from + i],
+                    )
+            else:
+                # can't move
+                pass
+
+            b = b - fsize
+        else:
+            b -= 1
+
+    res = sum([i * b for i, b in enumerate(fs) if b != -1])
+
+    return res
 
 
 print(part1())
