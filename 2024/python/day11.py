8ad3a85b48b5b2536b9b902909695ae8c78d2aa9
Harsh Narayan Jha <harshnj@proton.me>
2024-12-11 11:06:01 +0530

    day11 part2 in python, both optimized

--- 2024/python/day11.py
+++ 2024/python/day11.py
@@ -5,29 +5,61 @@ def part1() -> int:
 
     blinks = 25
 
+    counts = dict.fromkeys(line, 0)
+    for v in line:
+        counts[v] += 1
+
     for _ in range(blinks):
-        print(_)
-        new_line = []
-        for num in line:
-            if num == 0:
-                new_line.append(1)
-            elif len(str(num)) % 2 == 0:
-                st = str(num)
-                mid = len(st) // 2
-                left, right = st[:mid], st[mid:]
-                new_line.extend([int(left), int(right)])
-            else:
-                new_line.append(2024 * num)
+        new_counts = {}
 
-        line = new_line
+        new_counts[1] = counts.get(0, 0)
 
-    return len(line)
+        for num, count in counts.items():
+            if num != 0:
+                s = str(num)
+                if len(s) % 2 == 1:
+                    new_counts[2024 * num] = new_counts.get(2024 * num, 0) + count
+                else:
+                    mid = len(s) // 2
+                    left, right = int(s[:mid]), int(s[mid:])
+                    new_counts[left] = new_counts.get(left, 0) + count
+                    new_counts[right] = new_counts.get(right, 0) + count
 
-    return 0
+            counts = new_counts
+
+    return sum(counts.values())
 
 
 def part2() -> int:
-    ...
+    line = None
+    with open("../data/input11.txt", "r") as inp:
+        line = list(map(int, inp.read().strip().split()))
+
+    blinks = 75
+
+    counts = dict.fromkeys(line, 0)
+    for v in line:
+        counts[v] += 1
+
+    for _ in range(blinks):
+        new_counts = {}
+
+        new_counts[1] = counts.get(0, 0)
+
+        for num, count in counts.items():
+            if num != 0:
+                s = str(num)
+                if len(s) % 2 == 1:
+                    new_counts[2024 * num] = new_counts.get(2024 * num, 0) + count
+                else:
+                    mid = len(s) // 2
+                    left, right = int(s[:mid]), int(s[mid:])
+                    new_counts[left] = new_counts.get(left, 0) + count
+                    new_counts[right] = new_counts.get(right, 0) + count
+
+            counts = new_counts
+
+    return sum(counts.values())
 
 
 print(part1())
